0f69795fa17d2a31c19b404deee14692a04aa083
Simone Rossi <simone.rossi@eurecom.fr>
2019-08-13 09:54:48 +0200

    [edit] New directory + mcmc
    Signed-off-by: Simone Rossi <simone.rossi@eurecom.fr>

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -11,16 +11,13 @@ import argparse
 if __name__ == '__main__':
 
     args = argparse.Namespace()
-    args.animate = False
-    args.save_tikz = True
+    args.animate = True
+    args.save_tikz = False
 
     n_dim = 2
     mean = np.array([1., 2.])
     cov = (np.array([[1, .9], [.9, 1]]))
     rng = np.random.RandomState(seed=12345)
-
-    print(mean)
-    print(cov)
 
     def pot_energy(pos):
         pos_minus_mean = pos - mean
@@ -33,14 +30,13 @@ if __name__ == '__main__':
     # Hamiltonian is separable therefore use explicit leapfrog integrator
     integrator = hmc.integrators.LeapfrogIntegrator(system, step_size=0.125)
 
-    # Use dynamic integration-time HMC implementation with multinomial
-    # sampling from trajectories
+    # Use Hybrid HMC implementation of trajectory sampling
     sampler = hmc.samplers.StaticMetropolisHMC(system, integrator, rng, n_step=5)
 
-    # Sample an initial position from zero-mean isotropic Gaussian
-    init_pos = np.array([-1., -1.])#rng.normal(size=n_dim)
+    # Sample an initial position
+    init_pos = np.array([-3., -3.])
 
-    # Sample a Markov chain with 1000 transitions
+    # Sample a Markov chain
     chains, chain_stats = sampler.sample_chain(250, init_pos)
 
     def loglikelihood(samples):
@@ -49,14 +45,12 @@ if __name__ == '__main__':
                       + np.log(np.linalg.det(cov)))
 
     samples = chains['pos']
-    nlikl = []
-    for i in range(1, len(samples)+1):
-        nlikl.append(loglikelihood(samples[:i+1]).mean())
+    nlikl = [loglikelihood(samples[:i+1]).mean() for i in range(1, len(samples)+1)]
 
 
     N = 64
     X = np.linspace(-2, 4, N)
-    Y = np.linspace(-1, 5, N)
+    Y = np.linspace(-2, 5, N)
     X, Y = np.meshgrid(X, Y)
 
     # Pack X and Y into a single 3-dimensional array
@@ -73,25 +67,28 @@ if __name__ == '__main__':
     ax1.set_ylim(min(nlikl), max(nlikl))
 
     i = 1
-
-    if args.animate:
+    step = 2
+    if args.animate or args.save_gif:
         def animate(*args, **kwargs):
             global i
-            ax0.plot(chains['pos'][:i-1, 0], chains['pos'][:i-1, 1], '.', c='black')
-            ax0.plot(chains['pos'][i, 0], chains['pos'][i, 1], '.', c='r')
-            ax1.plot(nlikl[:i], color='#377eb8')
-            i += 1
+            ax0.plot(chains['pos'][:i, 0], chains['pos'][:i, 1], '.', c='black')
+            ax0.plot(chains['pos'][i:i+step, 0], chains['pos'][i:i+step, 1], '.', c='r')
+            ax1.plot(nlikl[:i+step], color='#377eb8')
+            i += step
             i = min(i, len(chains['pos'])-1)
             return ax0, ax1
 
 
         ani = animation.FuncAnimation(fig, animate, frames=250, interval=3, blit=False)
-        plt.show()
+        if args.animate:
+            plt.show()
+        if args.save_gif:
+            ani.save('gif/mcmc_animation.gif')
 
     if args.save_tikz:
         ax0.plot(chains['pos'][..., 0], chains['pos'][..., 1], '.', c='black')
         ax1.plot(nlikl, color='#377eb8')
-        tpl.save('test.tex',
+        tpl.save('tikz/mcmc_animation.tex',
                  figurewidth='\\figurewidth', figureheight='\\figureheight',
                  strict=True, externalize_tables=False, show_info=True)
         plt.show()
